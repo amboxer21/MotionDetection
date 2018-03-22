bbe7cd05322016b6a9c0a931ba56ac14ef95e99c
amboxer21 <amboxer21@gmail.com>
2018-03-22 13:11:16 -0400

    Removed html if condition.

--- LiveStream.py
+++ LiveStream.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 
-import cv2
-import time
-import Image
-import StringIO
-import threading
+import Image,time,cv2,sys
+import StringIO,threading
 
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from SocketServer import ThreadingMixIn
@@ -15,14 +12,14 @@ class CamHandler(BaseHTTPRequestHandler):
   def do_GET(self):
     if self.path.endswith('.mjpg'):
       self.send_response(200)
-      self.send_header('Content-type','multipart/x-mixed-replace; boundary=--jpgboundary')
+      self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=--jpgboundary')
       self.end_headers()
       while True:
         try:
-          rc,img = capture.read()
+          rc, img = capture.read()
           if not rc:
             continue
-          imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+          imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
           jpg = Image.fromarray(imgRGB)
           tmpFile = StringIO.StringIO()
           jpg.save(tmpFile,'JPEG')
@@ -35,22 +32,13 @@ class CamHandler(BaseHTTPRequestHandler):
         except KeyboardInterrupt:
           break
       return
-    if self.path.endswith('.html'):
-      self.send_response(200)
-      self.send_header('Content-type','text/html')
-      self.end_headers()
-      self.wfile.write('<html><head></head><body>')
-      self.wfile.write('<img src="http://0.0.0.0:8080/cam.mjpg"/>')
-      self.wfile.write('</body></html>')
-      return
-
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
   """Handle requests in a separate thread."""
 
 def main():
   global capture
-  capture = cv2.VideoCapture(1)
+  capture = cv2.VideoCapture(int(sys.argv[1]))
   capture.set(3,320)
   capture.set(4,320)
   global img
@@ -59,7 +47,8 @@ def main():
     print "server started"
     server.serve_forever()
   except KeyboardInterrupt:
-    capture.release()
+    del(capture)
+    #capture.release()
     server.socket.close()
 
 if __name__ == '__main__':
