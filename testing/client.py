9347385e7fad0c8adf51e8013d1359f39ae257e5
amboxer21 <amboxer21@gmail.com>
2018-03-27 12:57:23 -0400

    Finally got everything working together with the server and android app.

--- testing/client.py
+++ testing/client.py
@@ -5,15 +5,15 @@ from optparse import OptionParser
 
 class Client():
 
-    def __init__(self,ip='127.0.0.1',port=5050,command='probe'):
+    def __init__(self,ip='127.0.0.1',port=5051,command='probe'):
 
         parser = OptionParser()
         parser.add_option("-c",
             "--command", dest='command', help='"Command to send server. Defaults to \"probe\""',default='probe')
         parser.add_option("-i",
-            "--ip-addr", dest='ip', help='"The servers ip address. Defaults to 127.0.0.1."')
+            "--ip-addr", dest='ip', help='"The servers ip address. Defaults to 127.0.0.1."',default='127.0.0.1')
         parser.add_option("-p",
-            "--port", dest='port', help='"Deafults to port 5050"', type="int", default=5050)
+            "--port", dest='port', help='"Deafults to port 5051"', type="int", default=5051)
         (options, args) = parser.parse_args()
 
         self.ip = options.ip
@@ -30,13 +30,15 @@ class Client():
         print("Socket created.")
 
         try:
-            #s.connect('174.57.49.30','5050')
-            s.connect((socket.gethostbyaddr(self.ip),int(self.port)))
-            s.send(str(self.command))
-            print("Server: " + s.recv(1024))
-            s.close()
+            s.connect(('192.168.1.13',50050))
+            #s.connect(('174.57.49.30',5052))
+            #s.connect((str(self.ip),int(self.port)))
         except Exception as e:
             print("Connect exception Error => " + str(e))
+
+        s.send(str(self.command))
+        print("Server: " + s.recv(1024))
+        s.close()
 
 if __name__ == '__main__':
     c = Client()
