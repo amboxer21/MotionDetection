0bbb26bd2a043eaf3bf204e58b112255082e5d21
amboxer21 <amboxer21@gmail.com>
2018-04-16 19:16:53 -0400

    Added more db functions.

--- sqlite_test.py
+++ sqlite_test.py
@@ -7,7 +7,7 @@ class SQLDB():
     def __init__(self,db_name):
         self.db = sqlite3.connect(db_name)
     
-    def selectAll(self):
+    def select_all(self):
         while True:
             with self.db:
                 self.db.row_factory = sqlite3.Row
@@ -18,29 +18,42 @@ class SQLDB():
                     return data
                 except sqlite3.OperationalError as e:
                     if re.search('no such table:', str(e), re.I | re.M):
-                        print("Exception e => " + str(e))
                         cursor.execute('Create table motion(id INTEGER PRIMARY KEY NOT NULL, name TEXT, state TEXT)')
                         cursor.execute("Insert into motion (name, state) values('kill_camera','False')")
                         cursor.execute("Insert into motion (name, state) values('kill_motion','False')")
                     elif re.search('no such column:', str(e), re.I | re.M):
-                        print("Exception e => " + str(e))
                         cursor.execute("Insert into motion (name, state) values('kill_camera','False')")
                         cursor.execute("Insert into motion (name, state) values('kill_motion','False')")
                     self.db.commit()
 
-    def killCamera(self):
-        for d in self.selectAll():
+    def insert(self,column,value):
+        with self.db:
+            cursor = self.db.cursor()
+            try:
+                cursor.execute("insert into " + column + " values(" + value + ");")
+            except Exception as e:
+                pass
+            self.db.commit()
+
+    def update(self,column,value):
+        with self.db:
+            cursor = self.db.cursor()
+            try:
+                cursor.execute("update motion set state = '" + value + "' where name = '" + column + "';")
+            except Exception as e:
+                pass
+            self.db.commit()
+
+    def kill_camera_state(self):
+        for d in self.select_all():
             if re.search('kill_camera', str(d["name"])):
                 return d['state']
 
-    def killMotion(self):
-        for d in self.selectAll():
+    def kill_motion_state(self):
+        for d in self.select_all():
             if re.search('kill_camera', str(d["name"])):
                 return d['state']
 
 if __name__ == '__main__':
-    sqldb = SQLDB('test.db')
-    sqldb.selectAll()
-    print "killCamera() => " + str(sqldb.killCamera())
-    for d in sqldb.selectAll():
-        print "%s %s %s" % (d["id"], d["name"], d["state"])
+    sqldb = SQLDB('motiondetection.db')
+    sqldb.select_all()
