2a041efca5b72943f755651b71e1bcf7192fccf5
Anthony Guevara <amboxer21@gmail.com>
2018-09-11 14:28:30 +0000

    Modified the deamon monitoring script.

--- cronjob/is_motiondetection_running.sh
+++ cronjob/is_motiondetection_running.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [[ `ps aux | egrep -i "[0-9]*:[0-9]* /usr/bin/python .*/MotionDetection/motiondetection.py -[pe] .* -[pe] .*$" | wc -l` < 1 ]]; then 
-	/usr/bin/python /home/pi/Documents/Python/MotionDetection/MotionDetection.py -e 'sshmonitorapp@gmail.com' -p 'GHOST21ghost';
-fi
-
-if [[ `ps aux | egrep -i "[0-9]*:[0-9]* /usr/bin/python .*/MotionDetection/motiondetection.py -[pe] .* -[pe] .*$" | wc -l` > 1 ]]; then 
-	kill -9 `ps aux | egrep -i "motiondetection.py" | awk '{print $2}'`;
+if [[ `/bin/ps aux | /bin/egrep -i "[0-9]{1,2}:[0-9]{1,2} /usr/bin/python /usr/local/bin/motiondetection.py" | wc -l` < 1 ]]; then 
+    logger -i -t motiondetection "Motiondetection is not running, starting motiondetection." -f /var/log/motiondetection.log
+    /usr/bin/python /usr/local/bin/motiondetection.py -e 'sshmonitorapp@gmail.com' -p 'hkeyscwhgxjzafvj' &
+elif [[ `/bin/ps aux | /bin/egrep -i "[0-9]{1,2}:[0-9]{1,2} /usr/bin/python /usr/local/bin/motiondetection.py" | wc -l` < 1 ]]; then 
+    logger -i -t motiondetection "Restarting motiondetection." -f /var/log/motiondetection.log
+    /usr/bin/sudo /bin/kill -9 `/bin/ps aux | /bin/egrep -i "[0-9]{1,2}:[0-9]{1,2} /usr/bin/python /usr/local/bin/motiondetection.py" | /usr/bin/awk '{print $2}'`;
+else
+    logger -i -t motiondetection "Motiondetection is running." -f /var/log/motiondetection.log
 fi
