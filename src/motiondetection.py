2fc063b502e9f9d651f4b6f81278ec11cf5b85c1
Anthony Guevara <amboxer21@gmail.com>
2018-09-18 18:42:05 +0000

    Updated Accepts decorator class.

--- src/motiondetection.py
+++ src/motiondetection.py
@@ -24,24 +24,26 @@ from SocketServer import ThreadingMixIn
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
 class Accepts(object):
-
+    
     @staticmethod
     def boolean(func):
         def wrapper(*args):
-            length = len(args)
-            for index in range(1,(int(length))):
-                if not str(args[int(index)]) in ('True','False'):
-                    raise TypeError('"' + str(args[int(index)]) + '" is not a bool type!')
+            for arg in args:
+                if re.search(r'<__main__',str(arg)) is not None:
+                    pass
+                elif not str(arg) in ('True','False'):
+                    raise TypeError('"' + str(arg) + '" is not a bool type!')
             return func
         return wrapper
-
+    
     @staticmethod
     def integer(func):
         def wrapper(*args):
-            length = len(args)
-            for index in range(1,(int(length))):
-                if re.search(r'\d+\b', args[int(index)]) is None:
-                    raise TypeError('"' + str(args[int(index)]) + '" is not an integer!')
+            for arg in args:
+                if re.search(r'<__main__',str(arg)) is not None:
+                    pass
+                elif re.search(r'\d+\b', str(arg)) is None:
+                    raise TypeError('"' + str(arg) + '" is not an integer!')
             return func
         return wrapper
 
