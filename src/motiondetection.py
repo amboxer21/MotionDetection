cc3de08b014afaa6b2a40aaf2d93888285d3f966
amboxer21 <amboxer21@gmail.com>
2018-09-19 11:43:19 -0400

    Updated accepts class.

--- src/motiondetection.py
+++ src/motiondetection.py
@@ -25,24 +25,46 @@ from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
 class Accepts(object):
 
-    @staticmethod
-    def boolean(func):
+    @classmethod
+    def boolean(cls,func):
+        arg_count = func.__code__.co_argcount
         def wrapper(*args):
-            length = len(args)
-            for index in range(1,(int(length))):
-                if not str(args[int(index)]) in ('True','False'):
-                    raise TypeError('"' + str(args[int(index)]) + '" is not a bool type!')
-            return func
+            for arg in args:
+                if re.search(r'<__main__',str(arg)) is not None:
+                    pass
+                elif not isinstance(arg, bool):
+                    raise TypeError('"' + str(arg) + '" is not a bool type!')
+            if int(arg_count) > 1:
+                return func(cls,args)
+            return func(args)
         return wrapper
-
-    @staticmethod
-    def integer(func):
+    
+    @classmethod
+    def integer(cls,func):
+        arg_count = func.__code__.co_argcount
         def wrapper(*args):
-            length = len(args)
-            for index in range(1,(int(length))):
-                if re.search(r'\d+\b', args[int(index)]) is None:
-                    raise TypeError('"' + str(args[int(index)]) + '" is not an integer!')
-            return func
+            for arg in args:
+                if re.search(r'<__main__',str(arg)) is not None:
+                    pass
+                elif not isinstance(arg, int):
+                    raise TypeError('"' + str(arg) + '" is not an integer!')
+            if int(arg_count) > 1:
+                return func(cls,args)
+            return func(args)
+        return wrapper
+    
+    @classmethod
+    def string(cls,func):
+        arg_count = func.__code__.co_argcount
+        def wrapper(*args):
+            for arg in args:
+                if re.search(r'<__main__',str(arg)) is not None:
+                    pass
+                elif not isinstance(arg, str):
+                    raise TypeError('"' + str(arg) + '" is not a string!')
+            if int(arg_count) > 1:
+                return func(cls,args)
+            return func(args)
         return wrapper
 
 class Logging(object):
