9c107e075a54ef939790ca342722043b1d26b959
Anthony Guevara <amboxer21@gmail.com>
2018-11-16 20:37:04 +0000

    updated heartbeat script.

--- src/testing/heart.py
+++ src/testing/heart.py
@@ -7,19 +7,17 @@ import signal
 
 class Heart(object):
 
-    __motion_pid__ = None
-    __stream_pid__ = None
-
     @classmethod
     def beat(cls):
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.settimeout(1)
             sock.connect(('127.0.0.1',50050))
-            sock.sendall(b'ping')
-            Heart.__motion_pid__ = sock.recv(1024)
+            sock.send('ping')
+            sock.close()
             return True
         except Exception as e:
+            #print("Exception e => "+str(e))
             return False
 
 if __name__ == '__main__':
@@ -27,8 +25,10 @@ if __name__ == '__main__':
         try:
             time.sleep(10)
             if Heart.beat():
+                print('Alive!')
                 pass
             else:
-                os.kill(int(Heart.__motion_pid__), signal.SIGTERM)
+                print('Dead!')
+                pass
         except OSError:
             pass
