092c9b97e8b26cb1d5e555cd3c23ad907d529fa7
Anthony Guevara <amboxer21@gmail.com>
2018-11-16 22:02:44 +0000

    Now handling pid list in heartbeat script

--- src/testing/heart.py
+++ src/testing/heart.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
 import os
+import re
 import time
 import socket
 import signal
 
 class Heart(object):
+
+    __pids__ = []
+
+    @classmethod
+    def format_data(cls,data):
+        data = re.match('(\[)(.*)(, )(.*)(, )(.*)(\])', data, re.M | re.I)
+        if data is not None:
+            return [data.group(2),data.group(4),data.group(6)]
 
     @classmethod
     def beat(cls):
@@ -14,10 +23,13 @@ class Heart(object):
             sock.settimeout(1)
             sock.connect(('127.0.0.1',50050))
             sock.send('ping')
+            data = sock.recv(1024)
+            if data is not None:
+                Heart.__pids__ = Heart.format_data(data)
             sock.close()
             return True
         except Exception as e:
-            #print("Exception e => "+str(e))
+            print("Exception e: "+str(e))
             return False
 
 if __name__ == '__main__':
@@ -29,6 +41,7 @@ if __name__ == '__main__':
                 pass
             else:
                 print('Dead!')
+                [os.kill(int(pid), signal.SIGTERM) for pid in Heart.__pids__]
                 pass
         except OSError:
             pass
