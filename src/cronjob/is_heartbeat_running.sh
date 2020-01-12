226590577613cdd81ef47b4fb65101eb534cb19b
Anthony Guevara <amboxer21@gmail.com>
2020-01-12 16:05:38 -0500

    Update is_heartbeat_running.sh

--- src/cronjob/is_heartbeat_running.sh
+++ src/cronjob/is_heartbeat_running.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ `/bin/ps aux | /usr/bin/awk '/^root.*:[0-9]{1,2}.*heart.py/{if($11 !~ /sudo|vim|awk/) print $2}' | wc -l` -eq 0 ]]; then 
+if [[ `/bin/ps aux | /usr/bin/awk '/^root.*:[0-9].*heart.py/{if($11 !~ /awk/) print $2}' | wc -l` -eq 0 ]]; then 
     /usr/bin/sudo logger -i -t heartbeat "Heartbeat is not running, starting heartbeat." -f /var/log/motiondetection.log
     /usr/bin/sudo /usr/bin/python3 /usr/local/bin/heart.py -e 'sshmonitorapp@gmail.com' -p 'hkeyscwhgxjzafvj' &
-elif [[ `/bin/ps aux | /usr/bin/awk '/^root.*:[0-9]{1,2}.*heart.py/{if($11 !~ /sudo|vim|awk/) print $2}' | wc -l` -gt 1 ]]; then 
+elif [[ `/bin/ps aux | /usr/bin/awk '/^root.*:[0-9].*heart.py/{if($11 !~ /awk/) print $2}' | wc -l` -gt 2 ]]; then 
     /usr/bin/sudo logger -i -t heartbeat "Restarting heartbeat." -f /var/log/motiondetection.log
-    /usr/bin/sudo /bin/kill -9 `/bin/ps aux | /bin/egrep -i "[0-9]{1,2}:[0-9]{1,2} /usr/bin/python3 /usr/local/bin/heart.py" | /usr/bin/awk '{print $2}'`;
+    /usr/bin/sudo /bin/kill -9 `/bin/ps aux | /usr/bin/awk '/^root.*:[0-9].*heart.py/{if($11 !~ /awk/) print $2}'`;
     /usr/bin/sudo /usr/bin/python3 /usr/local/bin/heart.py -e 'sshmonitorapp@gmail.com' -p 'hkeyscwhgxjzafvj' &
-elif [[ `/bin/ps aux | /usr/bin/awk '/^root.*:[0-9]{1,2}.*heart.py/{if($11 !~ /sudo|vim|awk/) print $2}' | wc -l` -eq 1 ]]; then 
+elif [[ `/bin/ps aux | /usr/bin/awk '/^root.*:[0-9].*heart.py/{if($11 !~ /awk/) print $2}' | wc -l` -eq 2 ]]; then 
     /usr/bin/sudo logger -i -t heartbeat "Heartbeat is running." -f /var/log/motiondetection.log
 fi
