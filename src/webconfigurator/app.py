43a62710dfc0680e5cdecf03e41814dd040ebf79
amboxer21 <amboxer21@gmail.com>
2020-08-11 13:36:30 -0400

    Passing picture ids in post requests.

--- src/webconfigurator/app.py
+++ src/webconfigurator/app.py
@@ -8,26 +8,20 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/delete_selected_photos/<images>',methods=['POST'])
-def delete_selected_photos(images):
-    print(request.data)
-    #print(request.form['static/capture52.png'])
-    images = []
-    path  = 'static'
-    files = [f for f in listdir(path) if isfile(join(path, f))]
+def images(photos=[]):
+    files = [f for f in listdir('static') if isfile(join('static', f))]
     for f in files:
-        images.append(os.path.join(path, f))
-    return render_template("test.html", images=images)
+        photos.append(os.path.join('static', f))
+    return photos
+
+@app.route('/delete_selected_photos',methods=['POST'])
+def delete_selected_photos():
+    print(request.form['image'])
+    return render_template("test.html", images=images())
 
 @app.route('/')
 def show_index():
-    images = []
-    #path  = '/home/anthony/Pictures/capture'
-    path  = 'static'
-    files = [f for f in listdir(path) if isfile(join(path, f))]
-    for f in files:
-        images.append(os.path.join(path, f))
-    return render_template("test.html", images=images)
+    return render_template("test.html", images=images())
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
