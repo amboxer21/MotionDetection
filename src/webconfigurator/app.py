ffcc0e85d11f974c1a6d16820a27b7ab1ab42f46
amboxer21 <amboxer21@gmail.com>
2020-08-11 13:57:12 -0400

    Now deleting the photos.

--- src/webconfigurator/app.py
+++ src/webconfigurator/app.py
@@ -9,6 +9,7 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 def images(photos=[]):
+    photos.clear()
     files = [f for f in listdir('static') if isfile(join('static', f))]
     for f in files:
         photos.append(os.path.join('static', f))
@@ -16,7 +17,14 @@ def images(photos=[]):
 
 @app.route('/delete_selected_photos',methods=['POST'])
 def delete_selected_photos():
-    print(request.form['image'])
+
+    cwd  = os.getcwd()
+    img  = request.form['image']
+    path = os.path.join(cwd, img)
+
+    print(img)
+    if os.path.isfile(path) or os.path.islink(path):
+        os.remove(path)
     return render_template("test.html", images=images())
 
 @app.route('/')
