977bac9b9365eeb76cd1d88c8b90bb83879cdd22
amboxer21 <amboxer21@gmail.com>
2020-08-12 09:53:06 -0400

    Added navbar to photos.py

--- src/webconfigurator/photos.py
+++ src/webconfigurator/photos.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template
 
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+photos = Flask(__name__)
 
 def images(photos=[]):
     photos.clear()
@@ -17,7 +17,7 @@ def images(photos=[]):
             photos.append(os.path.join('static', f))
     return photos
 
-@app.route('/delete_selected_photos',methods=['POST'])
+@photos.route('/delete_selected_photos',methods=['POST'])
 def delete_selected_photos():
 
     image  = request.form['image']
@@ -38,9 +38,9 @@ def delete_selected_photos():
 
     return render_template("test.html", images=images())
 
-@app.route('/')
+@photos.route('/')
 def show_index():
     return render_template("test.html", images=images())
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    photos.run(debug=True, host='0.0.0.0')
