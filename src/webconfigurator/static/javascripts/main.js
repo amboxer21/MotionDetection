1c54f8320ee2c7ff008e486010d63f81c620d2d8
amboxer21 <amboxer21@gmail.com>
2020-08-13 21:47:05 -0400

    Consolidated js into one static file

---
+++ src/webconfigurator/static/javascripts/main.js
@@ -0,0 +1,36 @@
+$(function() {
+
+    var url = new URL(window.document.location);
+
+    $('a.nav-link.home').click(function() {
+        window.location.replace(url.origin);
+    });
+    $('a.nav-link.logs').click(function() {
+        window.location.replace(url.origin+'/logs');
+    });
+    $('a.nav-link.photos').click(function() {
+        window.location.replace(url.origin+'/photos');
+    });
+    $('a.nav-link.configure').click(function() {
+        window.location.replace(url.origin+'/configure');
+    });
+
+    $('button.btn.btn-default.delete').click(function() {
+        document.getElementsByName('image').forEach(function(e) {
+            if(e.checked && (e.id !== '.hidden' || e.id !== 'on')) {
+                $.ajax({
+                    url: '/delete_selected_photos',
+                    data: {image : e.id},
+                    type: 'POST',
+                    success: function(response) {
+                        console.log(response);
+                    },
+                    error: function(error) {
+                        console.log(error);
+                    }
+                });
+            }
+        });
+    });
+
+});
