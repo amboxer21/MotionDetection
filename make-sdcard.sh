33b0f842ad0589d1b475ca03fe57cdfd9c057678
amboxer21 <amboxer21@gmail.com>
2020-10-27 11:20:29 -0400

    Refactored make-sdcard.sh

--- make-sdcard.sh
+++ make-sdcard.sh
@@ -11,18 +11,18 @@ if [[ $mountpoint ]] ; then
     sudo umount -R $mountpoint;
 fi
 
-echo "=> Partitioning sdcard.";
+echo "[ INFO ] Partitioning sdcard.";
 for n in {1..4}; do parted -a optimal /dev/mmcblk0 rm $n 2> /dev/null; done
 sudo parted -a optimal /dev/mmcblk0 mkpart primary fat32 0% 513MB
 sudo parted -a optimal /dev/mmcblk0 mkpart primary ext4 513MB 100%
 
-echo "=> Creating FAT32 filesystem on /dev/mmcblk0p1.";
+echo "[ INFO ] Creating FAT32 filesystem on /dev/mmcblk0p1.";
 echo 'y' | sudo mkfs.vfat -F32 /dev/mmcblk0p1
 
-echo "=> Creating EXT4 filesystem on /dev/mmcblk0p2.";
+echo "[ INFO ] Creating EXT4 filesystem on /dev/mmcblk0p2.";
 echo 'y' | sudo mkfs.ext4 /dev/mmcblk0p2
 
-echo "=> Checking if mountpoint /mnt/pi exists.";
+echo "[ INFO ] Checking if mountpoint /mnt/pi exists.";
 if [[ -e /mnt/pi ]] ; then
     echo "[ INFO ] Mountpoint /dev/pi exists.";
 else
@@ -30,10 +30,10 @@ else
     echo '[ WARNING ] /mnt/pi doesnt exist - creating it now.';
 fi
 
-echo "=> Mounting /dev/mmcblk0p2 on /mnt/pi";
+echo "[ INFO ] Mounting /dev/mmcblk0p2 on /mnt/pi";
 sudo mount /dev/mmcblk0p2 /mnt/pi;
 
-echo "=> Checking if mountpoint /mnt/pi/boot exists.";
+echo "[ INFO ] Checking if mountpoint /mnt/pi/boot exists.";
 if [[ -e /mnt/pi/boot ]] ; then
     echo "[ INFO ] Mountpoint /dev/pi/boot exists.";
 else
@@ -41,11 +41,11 @@ else
     echo '[ WARNING ] /mnt/pi/boot doesnt exist - creating it now.';
 fi
 
-echo "=> Mounting /dev/mmcblk0p1 on /mnt/pi/boot";
+echo "[ INFO ] Mounting /dev/mmcblk0p1 on /mnt/pi/boot";
 sudo mount /dev/mmcblk0p1 /mnt/pi/boot;
 
-echo "=> Unpacking tarball onto your sdcard.";
-tar -xzvf pi.tar.gz -C /mnt/pi/ ;
+echo "[ INFO ] Unpacking tarball onto your sdcard.";
+tar -xzf pi.tar.gz -C /mnt/pi/ ;
 
-echo "=> Unmounting your sdcard now.";
+echo "[ INFO ] Unmounting your sdcard now.";
 sudo umount -R /mnt/pi ;
